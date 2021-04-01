b89e7254e7c64e601e93c154f55b56061c559635
Anirudh <anirudh217sharma@gmail.com>
2021-04-01 15:10:55 -0500

    Added Rules and constraints for the puzzle

---
+++ Unequal.py
@@ -0,0 +1,10 @@
+"""
+Name : Unequal (Puzzle)
+
+Fill in the grid with numbers from 1 to the grid size, so that every number appears exactly once in each row and column, and so that all the < signs represent true inequalities (i.e. the number at the pointed end is smaller than the number at the open end).
+
+A valid puzzle must have at least a single row or column or a diagonal in numerical order. There is no constraints on the corners.
+
+As a result now there is not restriction to work on a 4 * 4 grid so the implementation would be for a general n * n grid.
+"""
+
