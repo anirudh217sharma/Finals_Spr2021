98955315850f05916600ef39e14b629120523032
Anirudh <anirudh217sharma@gmail.com>
2021-04-20 22:43:12 -0500

    Unequal.py :-> Solver first attempt : working on the testing grid. NOTE : Right now only the original constraints are applied not the special numerical one. Function still needs testing

--- Unequal.py
+++ Unequal.py
@@ -358,28 +358,9 @@ def solver(grid):
             if solver(grid):
                 return True
 
-            grid[move[0]][move[1]] = ''
+        grid[move[0]][move[1]] = ''
 
     return False
-
-
-# def solve(bo):
-#     find = find_empty(bo)
-#     if not find:
-#         return True
-#     else:
-#         row, col = find
-#
-#     for i in range(1, 10):
-#         if valid(bo, i, (row, col)):
-#             bo[row][col] = i
-#
-#             if solve(bo):
-#                 return True
-#
-#             bo[row][col] = 0
-#
-#     return False
 
 
 print(test_puzzle)
